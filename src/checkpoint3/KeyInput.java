e3c359a1c32b5c459f3018588935f57935fd51c0
Madison Smith <98797413+Madzswb21@users.noreply.github.com>
2025-04-28 00:43:37 -0700

    Add files via upload

---
+++ src/checkpoint3/KeyInput.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package checkpoint1;
+
+/**
+ * Static methods for keyboard & mouse input handling (WASD, arrow keys, space, shift, ESC).
+ */
+public class KeyInput {
+    
+}
